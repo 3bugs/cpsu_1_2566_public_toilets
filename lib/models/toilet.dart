ada85d159ff387a0e97a840707f411af3449005f
Promlert Lovichit <promlert@gmail.com>
2023-10-21 21:07:10 +0700

    Update

--- lib/models/toilet.dart
+++ lib/models/toilet.dart
@@ -1,19 +1,35 @@
+import 'package:collection/collection.dart';
+import 'review.dart';
+
 class Toilet {
+  final int id;
   final String name;
-  final double point;
+  final double averageRating;
   final double distance;
+  final List<Review> reviews;
 
   Toilet({
+    required this.id,
     required this.name,
-    required this.point,
+    required this.averageRating,
     required this.distance,
+    required this.reviews,
   });
 
   factory Toilet.fromJson(Map<String, dynamic> json) {
+    List<Review> reviews =
+        json['reviews'].map<Review>((item) => Review.fromJson(item)).toList();
+    var averageRating = 0.0;
+    if (reviews.isNotEmpty) {
+      averageRating = reviews.map((review) => review.rating).average;
+    }
+
     return Toilet(
+      id: json['id'],
       name: json['name'],
-      point: json['rating'],
+      averageRating: averageRating,
       distance: json['distance'],
+      reviews: reviews,
     );
   }
 }
